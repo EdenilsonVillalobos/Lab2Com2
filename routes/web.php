7e5f052d3bb06250c08e5d0cdecf562c8dc8aeeb
JonatanMoraga <88854031+JonatanMoraga@users.noreply.github.com>
2022-04-02 23:55:24 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,22 @@
+<?php
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EstudianteController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/estudiante/{id}/{codigo}/{nombre}/{apellido}/{telefono}', function ($id=0, $codigo='', $nombre='', $apellido='', $telefono='') {
+    return ('ID estudiante: '.$id. '; codigo: '. $codigo. '; nombre: '. $nombre. '; apellido: '. $apellido. '; telefono: '. $telefono);
+})->where('id', '[0-9]+');
+
+Route::resource('estudiante',EstudianteController::class);
